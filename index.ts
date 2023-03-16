275be7726b3ab925f976153ab3cd471aaecfb4fd
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-03-16 22:59:17 +0100

    .

--- index.ts
+++ index.ts
@@ -1,3 +1,22 @@
-import git from "isomorphic-git"
+// import git from "isomorphic-git"
+// import { createFsFromVolume, Volume } from "memfs"
 
-console.log(git, "git")
+// const fs = createFsFromVolume(new Volume())
+// console.log(fs)
+
+// create fs with some files
+// emulate a use case for sparse checkout
+
+// await git.clone({
+//   fs,
+//   http,
+//   dir,
+//   corsProxy: 'https://cors.isomorphic-git.org',
+//   url: 'https://github.com/isomorphic-git/isomorphic-git',
+//   ref: 'main',
+//   singleBranch: true,
+//   depth: 10
+// });
+
+// console.log(memfs)
+// console.log(git, "git")
