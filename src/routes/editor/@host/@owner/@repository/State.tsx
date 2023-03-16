2f33a660df6478ef27f39ae9bde1d111d58da655
Nikita Voloboev <nikita.voloboev@gmail.com>
2023-03-16 23:32:21 +0100

    .

--- src/routes/editor/@host/@owner/@repository/State.tsx
+++ src/routes/editor/@host/@owner/@repository/State.tsx
@@ -1,12 +1,21 @@
-import { JSXElement } from "solid-js"
+import { JSXElement, createSignal } from "solid-js"
+import { createFsFromVolume, Volume } from "memfs"
 
 // TODO: git clone --depth 1 --filter=blob:none --sparse
 // then git fetch rest to speed up clone
 
 // TODO: add sparse clone to isomorphic-git
 // https://github.com/isomorphic-git/isomorphic-git/issues/1735
+// https://git-scm.com/docs/git-sparse-checkout
+// read https://github.com/Byron/gitoxide/blob/79ed8752451ce621cf1ee69d3ca15deae3416798/gix-index/src/access/sparse.rs#L9
 
 // Initialize editor state from results of git clone.
 export function EditorStateProvider(props: { children: JSXElement }) {
+  const [fsChange, setFsChange] = createSignal(new Date())
+
+  const [fs, setFs] = createSignal<typeof import("memfs").fs>(
+    createFsFromVolume(new Volume())
+  )
+
   return <></>
 }
